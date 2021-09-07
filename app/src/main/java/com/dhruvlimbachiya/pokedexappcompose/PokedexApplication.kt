1f7d96860eb0f2a5f2afcb5f71ef71867debc9bc
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-07 14:46:45 +0530

    Added Custom Application class for Hilt

---
+++ app/src/main/java/com/dhruvlimbachiya/pokedexappcompose/PokedexApplication.kt
@@ -0,0 +1,11 @@
+package com.dhruvlimbachiya.pokedexappcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Dhruv Limbachiya on 07-09-2021.
+ */
+
+@HiltAndroidApp
+class PokedexApplication : Application()
