8df895dc7a99d59330fbc97a08d1054d237784f7
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-07 14:43:32 +0530

    Setup navigation for List & Detail screen

--- app/src/main/java/com/dhruvlimbachiya/pokedexappcompose/MainActivity.kt
+++ app/src/main/java/com/dhruvlimbachiya/pokedexappcompose/MainActivity.kt
@@ -3,6 +3,18 @@ package com.dhruvlimbachiya.pokedexappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.dhruvlimbachiya.pokedexappcompose.others.Constants.KEY_DOMINANT_COLOR
+import com.dhruvlimbachiya.pokedexappcompose.others.Constants.KEY_POKEMON_NAME
+import com.dhruvlimbachiya.pokedexappcompose.others.Screen
 import com.dhruvlimbachiya.pokedexappcompose.ui.theme.JetpackComposePokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,9 +22,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePokedexTheme {
-
+                val navController = rememberNavController() // Get the NavController.
+                Navigation(navController = navController)
             }
         }
     }
 }
 
+/**
+ * Composable function responsible for hosting or navigating to other screen(composable).
+ */
+@Composable
+fun Navigation(navController: NavHostController) {
+
+    NavHost(navController = navController, startDestination = Screen.PokemonListScreen.route) {
+        // Composable for PokemonListScreen
+        composable(route = Screen.PokemonListScreen.route) {
+
+        }
+
+        // Composable for PokemonDetailScreen.
+        composable(
+            // Route for navigating and passing argument to PokemonDetailScreen.
+            route = Screen.PokemonDetailScreen.route + "/dominantColor/pokemonName",
+            // list of arguments.
+            arguments = listOf(
+                navArgument(KEY_DOMINANT_COLOR) {
+                    type = NavType.IntType
+                },
+                navArgument(KEY_POKEMON_NAME) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val dominantColor = remember {
+                val color =
+                    it.arguments?.getInt(KEY_DOMINANT_COLOR)  // Get the value from the arguments
+                color?.let { Color(it) } // Create a color object from the int value.
+            }
+
+            val pokemonName = remember {
+                it.arguments?.getString(KEY_POKEMON_NAME) // Get the pokemon name from the arguments
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dhruvlimbachiya/pokedexappcompose/others/Constants.kt
@@ -0,0 +1,12 @@
+package com.dhruvlimbachiya.pokedexappcompose.others
+
+/**
+ * Created by Dhruv Limbachiya on 07-09-2021.
+ */
+
+object Constants {
+    const val POKEMON_LIST_SCREEN_ROUTE = "pokemon_list_screen"
+    const val POKEMON_DETAIL_SCREEN_ROUTE = "pokemon_detail_screen"
+    const val KEY_DOMINANT_COLOR = "dominantColor"
+    const val KEY_POKEMON_NAME = "pokemonName"
+}
