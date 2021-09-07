53eaf5a87e84812504dba329a2a8bd229c7cf386
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-07 17:49:59 +0530

    added api & repo dependency in AppModule.kt

--- app/src/main/java/com/dhruvlimbachiya/pokedexappcompose/di/AppModule.kt
+++ app/src/main/java/com/dhruvlimbachiya/pokedexappcompose/di/AppModule.kt
@@ -1,8 +1,15 @@
 package com.dhruvlimbachiya.pokedexappcompose.di
 
+import com.dhruvlimbachiya.pokedexappcompose.BuildConfig
+import com.dhruvlimbachiya.pokedexappcompose.data.remote.PokeApi
+import com.dhruvlimbachiya.pokedexappcompose.repository.PokemonRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 /**
  * Created by Dhruv Limbachiya on 07-09-2021.
@@ -11,4 +18,17 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePokeApi(): PokeApi = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BuildConfig.POKEMON_API_BASE_URL)
+        .build()
+        .create(PokeApi::class.java)
+
+
+    @Singleton
+    @Provides
+    fun providePokemonRepository(pokeApi: PokeApi) = PokemonRepository(pokeApi)
 }
