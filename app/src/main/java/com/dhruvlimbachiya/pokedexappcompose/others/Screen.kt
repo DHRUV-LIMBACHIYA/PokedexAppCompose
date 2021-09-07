088ed89d8b209d6705ee478d39d43e1c336c41b5
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-07 14:42:25 +0530

    Screens for navigation

---
+++ app/src/main/java/com/dhruvlimbachiya/pokedexappcompose/others/Screen.kt
@@ -0,0 +1,27 @@
+package com.dhruvlimbachiya.pokedexappcompose.others
+
+import com.dhruvlimbachiya.pokedexappcompose.others.Constants.POKEMON_DETAIL_SCREEN_ROUTE
+import com.dhruvlimbachiya.pokedexappcompose.others.Constants.POKEMON_LIST_SCREEN_ROUTE
+
+/**
+ * Created by Dhruv Limbachiya on 07-09-2021.
+ */
+
+/**
+ * Screen sealed class for all navigation screens.
+ */
+sealed class Screen(val route: String) {
+    object PokemonListScreen : Screen(POKEMON_LIST_SCREEN_ROUTE)
+    object PokemonDetailScreen : Screen(POKEMON_DETAIL_SCREEN_ROUTE)
+
+    /**
+     * Helper function for building a route with arguments.
+     */
+    fun withArgs(vararg args: String) = buildString {
+        append(POKEMON_DETAIL_SCREEN_ROUTE) // Route - main url
+        args.forEach { arg ->
+            append("/$arg") // append arguments on route.
+        }
+
+    }
+}
